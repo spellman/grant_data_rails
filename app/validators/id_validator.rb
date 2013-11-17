a692c7ff4d7010653c8854d670b50426264f929f
Cort Spellman <spellman.cort@gmail.com>
2013-11-17 17:19:52 -0600

    Make custom validator to verify foreign key model from *_id attribute.

---
+++ app/validators/id_validator.rb
@@ -0,0 +1,14 @@
+class IdValidator < ActiveModel::EachValidator
+  def validate_each record, attribute, value
+    unless valid_id? id_attr: attribute, id: value
+      record.errors.add attribute, "must be valid"
+    end
+  end
+
+  # private
+  def valid_id? id_attr: nil, id: nil
+    attr_name  = id_attr.to_s.sub /_id$/, ""
+    attr_class = attr_name.humanize.titlecase.gsub("\s", "").constantize
+    attr_class.exists? id
+  end
+end
