9786900e52def4926ac44399a5aecb190c7dd5f6
Cort Spellman <spellman.cort@gmail.com>
2015-11-09 18:38:02 -0600

    Simplify and clean up Record form-object.

--- app/form_objects/record.rb
+++ app/form_objects/record.rb
@@ -3,27 +3,14 @@ class Record
   CouldNotSaveAllModels = Class.new(StandardError)
 
   def self.model_names
-    [
-      "a1c",
-      "acr",
-      "blood_pressure",
-      "bun_and_creatinine",
-      "cholesterol",
-      "ckd_stage",
-      "eye_exam",
-      "foot_exam",
-      "measurements",
-      "testosterone"
-    ]
+    Patient.record_types.map { |r| r.model_name.singular }
   end
 
-  attr_accessor :patient_id, :models, *self.model_names.map(&:to_sym)
+  attr_accessor :models, *self.model_names.map(&:to_sym)
 
-  def initialize attrs = default_attributes
-    attrs = Hash[attrs.map { |k, v| [k.to_s, v] }]
-    @patient_id = attrs.delete "patient_id"
-    @models = []
-    initialize_models attrs
+  def initialize(params = {})
+    @models = initialize_models(default_attributes.merge(params))
+    @models.each do |m| self.send("#{m.model_name.singular}=", m) end
   end
 
   validate :some_model_not_blank
@@ -37,22 +24,18 @@ class Record
 
   # private
   def default_attributes
-    Hash[self.class.model_names.map { |model_name| [model_name, {}] }]
+    Hash[self.class.model_names.zip([{}].cycle)]
   end
 
-  def initialize_models params
-    params.each do |model_name, model_attrs|
-      initialize_model name: model_name,
-                       attributes: model_attrs.merge({"patient_id" => patient_id})
+  def initialize_models(params)
+    patient_id = params.delete("patient_id")
+
+    params.map do |name, attrs|
+      model_class = Patient.record_types.select { |r|
+        r.model_name.singular == name
+      }.first
+      model_class.new(attrs.merge({"patient_id" => patient_id})).localized
     end
-  end
-
-  def initialize_model name: nil, attributes: nil
-    model_class = name.titlecase.gsub("\s", "").constantize
-    model = model_class.new
-    model.localized.assign_attributes attributes
-    models << model
-    send "#{name}=", model
   end
 
   def try_save_models
@@ -68,13 +51,13 @@ class Record
   def save_models!
     ActiveRecord::Base.transaction do
       save_results = []
-      non_blank_models.each { |model| save_results << model.save }
-      raise CouldNotSaveAllModels unless save_results.all?
+      non_blank_models.each do |model| save_results << model.save end
+      raise(CouldNotSaveAllModels) unless save_results.all?
     end
   end
 
   def some_model_not_blank
-    errors.add :base, "Nothing to save! Please enter some patient data." if non_blank_models.empty?
+    errors.add(:base, "Nothing to save! Please enter some patient data.") if non_blank_models.empty?
   end
 
   def aggregate_model_errors
@@ -91,7 +74,7 @@ class Record
     models.reject { |model| blank_model? model }
   end
 
-  def blank_model? model
+  def blank_model?(model)
     model.attributes.reject { |k, v| k == "patient_id" }.all? { |k, v| v.blank? }
   end
 end
