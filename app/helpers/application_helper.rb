780326e0cf1029824fcfd4889867aa8d206cd809
Cort Spellman <spellman.cort@gmail.com>
2015-11-18 18:19:52 -0600

    Convert forms and errors to rails-bootstrap-forms.
    I converted the record form and the various record-type forms to use the
    rails-bootstrap-forms wrappers. Other forms had already been converted.
    
    I converted the error messages to be displayed with each form field, via
    rails-bootstrap-forms, instead of in a list above the form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,21 @@
 module ApplicationHelper
-  def conjugate_be(object_count)
+  def pluralize_word(object_count, singular, plural)
     case object_count
-    when 1 then "is"
-    else "are"
+    when 1 then singular
+    else plural
+    end
+  end
+
+  def form_error_message(error_count)
+    "There #{pluralize_word(error_count, "is", "are")} #{pluralize(error_count, "error")}; please fix #{pluralize_word(error_count, "it", "them")} and re-submit."
+  end
+
+  def record_form_error_message(form_builder, record_errors)
+    if record_errors.messages.has_key?(:base)
+      form_builder.errors_on(:base)
+    else
+      form_builder.alert_message(form_error_message(record_errors.count),
+                                 error_summary: false)
     end
   end
 end
